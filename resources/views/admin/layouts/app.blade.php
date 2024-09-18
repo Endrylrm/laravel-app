9bb11c755f4ba46d47697c7d6da16c912bac4230
Endryl Richard Monteiro <endrylrm@hotmail.com>
2024-09-18 14:43:47 -0300

    Feature: list and add users.

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title') - Sistema</title>
+</head>
+
+<body>
+    <header>Teste header</header>
+    @yield('content')
+    <footer>Teste footer</footer>
+</body>
+
+</html>
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,38 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('admin.layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+@section('title', 'Lista de usuários')
 
-<body>
-    <h1>Usuários</h1>
-    <table>
-        <thead>
-            <tr>
-                <th>Nome</th>
-                <th>E-mail</th>
-                <th>Ações</th>
-            </tr>
-        </thead>
-        <tbody>
-            @forelse ($users as $user)
-            <tr>
-                <td>{{ $user->name }}</td>
-                <td>{{ $user->email }}</td>
-                <td>-</td>
-            </tr>
-            @empty
-            <tr>
-                <td colspan="100">Nenhum usuário cadastrado no banco de dados.</td>
-            </tr>
-            @endforelse
-        </tbody>
-    </table>
+@section('content')
+<h1>Usuários</h1>
 
-    {{ $users->links() }}
-</body>
+<a href="{{ route('users.create') }}">Novo</a>
 
-</html>
+<table>
+    <thead>
+        <tr>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Ações</th>
+        </tr>
+    </thead>
+    <tbody>
+        @forelse ($users as $user)
+        <tr>
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->email }}</td>
+            <td>-</td>
+        </tr>
+        @empty
+        <tr>
+            <td colspan="100">Nenhum usuário cadastrado no banco de dados.</td>
+        </tr>
+        @endforelse
+    </tbody>
+</table>
+
+{{ $users->links() }}
+@endsection
