d248ed90f5ab9b448036342b3e14fd6fcfc27cf5
Endryl Richard Monteiro <endrylrm@hotmail.com>
2024-09-18 16:53:15 -0300

    Feature: Only auth users access, Delete users and SoftDeletes.

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,17 +1,11 @@
 @extends('admin.layouts.app')
 
-@section('title', 'Cadastrar usuários')
+@section('title', 'Cadastrar usuário')
 
 @section('content')
 <h1>Novo Usuário</h1>
 
-<x-alert />
-
 <form action="{{ route('users.store') }}" method="post">
-    @csrf()
-    <input type="text" name="name" placeholder="Nome" value="{{ old('name') }}">
-    <input type="email" name="email" placeholder="E-mail" value="{{ old('email') }}">
-    <input type="password" name="password" placeholder="Senha">
-    <button type="submit">Enviar</button>
+    @include('admin.users.partials.form')
 </form>
 @endsection
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -22,7 +22,10 @@
         <tr>
             <td>{{ $user->name }}</td>
             <td>{{ $user->email }}</td>
-            <td><a href="{{ route('users.edit', $user->id) }}">Edit</a></td>
+            <td>
+                <a href="{{ route('users.edit', $user->id) }}">Edit</a>
+                <a href="{{ route('users.show', $user->id) }}">Detalhes</a>
+            </td>
         </tr>
         @empty
         <tr>
