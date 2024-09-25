3cd641b1c249cd1a38246b4f1e5d85f1ae311d4e
Endryl Richard Monteiro <endrylrm@hotmail.com>
2024-09-25 15:47:52 -0300

    Style: improve text of products and users.

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -6,7 +6,7 @@
 @include('products.partials.breadcrumb')
 <div class="py-6">
     <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight mb-4">
-        Editar o Produto {{ $product->name }}
+        Editar o Produto: {{ $product->name }}
     </h2>
 </div>
 <form action="{{ route('products.update', $product->id) }}" method="POST">
